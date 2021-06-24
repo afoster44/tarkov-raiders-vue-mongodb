d59a7ada6f55f0f6fd0ca2e26052a744ddd283c6
Alex Foster <alexfost94@gmail.com>
2021-06-24 12:16:55 -0600

    Setting up the single page for a raid

--- tarkov-site.client/src/components/Raids.vue
+++ tarkov-site.client/src/components/Raids.vue
@@ -1,4 +1,6 @@
 <template>
+  <router-link :to="{ name: 'RaidDetailsPage', params: { id: raid.id } }">
+  </router-link>
   <div class="raids-component card bg-info mt-5 border border-light">
     <p class="text-light ml-2 mt-1">
       {{ raid.title }}
---
+++ tarkov-site.client/src/pages/RaidDetailsPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container-fluid raid-details">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RaidDetailsPage'
+}
+</script>
+
+<style>
+
+</style>
